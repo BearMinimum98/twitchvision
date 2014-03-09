72d549c2f94ede8efc71fba90e30c7795ef1d888
woodenchopsticks <jonathan.z99@gmail.com>
2014-03-09 10:30:28 -0700

    //commenting

--- js/vision.js
+++ js/vision.js
@@ -86,39 +86,39 @@ $(window).ready(function() {
 
 
 
-var tag = document.createElement('script');
+// var tag = document.createElement('script');
 
   
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//       var firstScriptTag = document.getElementsByTagName('script')[0];
+//       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player1;
-      var player2;
-      var player3;
-      var player4;
+//       // 3. This function creates an <iframe> (and YouTube player)
+//       //    after the API code downloads.
+//       var player1;
+//       var player2;
+//       var player3;
+//       var player4;
      
 
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-        player1.mute();
-        player2.mute();
-        player3.mute();
-        player4.mute();
-      }
+//       // 4. The API will call this function when the video player is ready.
+//       function onPlayerReady(event) {
+//         event.target.playVideo();
+//         player1.mute();
+//         player2.mute();
+//         player3.mute();
+//         player4.mute();
+//       }
 
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          // setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player1.stopVideo();
-      }
+//       // 5. The API calls this function when the player's state changes.
+//       //    The function indicates that when playing a video (state=1),
+//       //    the player should play for six seconds and then stop.
+//       var done = false;
+//       function onPlayerStateChange(event) {
+//         if (event.data == YT.PlayerState.PLAYING && !done) {
+//           // setTimeout(stopVideo, 6000);
+//           done = true;
+//         }
+//       }
+//       function stopVideo() {
+//         player1.stopVideo();
+//       }
